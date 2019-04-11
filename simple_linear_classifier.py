2a2086f320cefa94a65e3959696372208867acbf
Yang Kyowon <kyowonhorang@gmail.com>
2019-04-11 12:04:07 +0900

    Modified RelaxationClassifier and plot datas

--- simple_linear_classifier.py
+++ simple_linear_classifier.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class LearningProc:
-    def __init__(self, init_weight, learning_rate, datas, class_w1_idx, class_w2_idx, epochs):
+    def __init__(self, init_weight, learning_rate, datas, class_w1_idx, class_w2_idx, epochs, bias):
         self.learning_rate = learning_rate
         self.class_1_data = datas[class_w1_idx]
         self.class_2_data = datas[class_w2_idx]
@@ -11,17 +11,18 @@ class LearningProc:
         self.class_w2_idx = class_w2_idx
         self.weight = init_weight
         self.epochs = epochs
+        self.bias = bias
 
     def get_missclasified_samples(self):
         missclasified = []
         for sample in self.class_1_data:
             classified = np.dot(self.weight, sample)
-            if (classified < 0):
+            if (classified <= self.bias):
                 # missclasified
                 missclasified.append([sample, -1])
         for sample in self.class_2_data:
             classified = np.dot(self.weight, sample)
-            if (classified >= 0):
+            if (classified >= self.bias):
                 missclasified.append([sample, 1])
         return missclasified
 
@@ -46,7 +47,7 @@ class PerceptronClassifier(LearningProc):
     def __init__(self, init_weight, learning_rate, datas, class_w1_idx, class_w2_idx, epochs):
         super().__init__(init_weight, learning_rate,
                         datas, class_w1_idx, class_w2_idx,
-                       epochs)
+                       epochs, 0.0)
 
     def d_criterion_func(self, missclassified_samples):
         crit = 0.0
@@ -55,30 +56,43 @@ class PerceptronClassifier(LearningProc):
         return crit
 
 class RelaxationClassifier(LearningProc):
-        def __init__(self, init_weight, learning_rate, datas, class_w1_idx, class_w2_idx, epochs, margin):
+        def __init__(self, init_weight, learning_rate, datas, class_w1_idx, class_w2_idx, epochs, bias):
             super().__init__(init_weight, learning_rate, 
                              datas, class_w1_idx, class_w2_idx, 
-                             epochs)
-            self.margin = margin
+                             epochs,
+                             bias)
+
+        def get_missclasified_samples(self):
+            missclasified = []
+            for sample in self.class_1_data:
+                classified = np.dot(self.weight, sample)
+                if (classified <= self.bias):
+                    # missclasified
+                    missclasified.append([sample, -1])
+            for sample in self.class_2_data:
+                classified = np.dot(self.weight, sample)
+                if (classified <= self.bias):
+                    missclasified.append([sample, 1])
+            return missclasified
 
         def d_criterion_func(self, missclassified_samples):
             # b is margin
             sum = np.zeros(shape = 2)
             for sample in missclassified_samples:
-                sum += ((np.dot(self.weight, sample[0]) - self.margin)/np.power(np.linalg.norm(sample[0]), 2))*sample[0]*sample[1]
+                sum += ((np.dot(self.weight, sample[0])-self.bias)/np.power(np.linalg.norm(sample[0]), 2))*sample[0]
             return sum
 
 class LMSClassifier(LearningProc):
-    def __init__(self, init_weight, learning_rate, datas, class_w1_idx, class_w2_idx, epochs, margin):
+    def __init__(self, init_weight, learning_rate, datas, class_w1_idx, class_w2_idx, epochs, bias):
             super().__init__(init_weight, learning_rate, 
                              datas, class_w1_idx, class_w2_idx, 
-                             epochs)
-            self.margin = margin
+                             epochs,
+                             bias)
 
     def d_criterion_func(self, missclassified_samples):
         sum = np.zeros(shape = 2)
         for sample in missclassified_samples:
-            sum += ((np.dot(self.weight, sample[0]) - self.margin) * sample[0])
+            sum += ((np.dot(self.weight, sample[0]) - self.bias) * sample[0])
         return sum
 
 def load_datas(filePath):
@@ -137,6 +151,24 @@ def median2D(samples):
     max = max2D(samples)
     return (min[0] + max[0]/2.0, min[1] + max[1]/2.0)
 
+def plot_samples(samples, mask, label):
+    class_samples_x = []
+    class_samples_y = []
+
+    for sample in samples:
+        class_samples_x.append(sample[0])
+        class_samples_y.append(sample[1])
+
+    plt.plot(class_samples_x, class_samples_y,
+            mask, label = label)
+
+def plot_datas(datas, masks):
+    label_idx = 1
+    for samples in datas:
+        label = 'class ' + str(label_idx)
+        plot_samples(samples, masks[label_idx-1], label)
+        label_idx += 1
+
 datas = load_datas('data.txt')
 print_datas(datas)
 
@@ -162,11 +194,16 @@ percep_classifier = PerceptronClassifier(init_weights[3],
                                         0.01, datas, 0, 1, 100 )
 #percep_classifier = PerceptronClassifier(np.array([0.0, 0.0]),
 #                                        0.0005, datas, 0, 1, 500 )
-percep_classifier.learn()
+#percep_classifier.learn()
 
 #relax_classifier = RelaxationClassifier(np.array([1.0, 1.5]), 0.001, datas, 0, 2, 1500, 0.1)
-relax_classifier = RelaxationClassifier(init_weights[3], 0.1, datas, 0, 2, 100, 1.0)
+# learning rate 를 0.1로 올리면 weight_vector가 어느 한 점으로 수렴해버림
+relax_classifier = RelaxationClassifier(init_weights[0], 0.01, datas, 0, 2, 100, 0.1)
 relax_classifier.learn()
 
 lms_classifier = LMSClassifier(init_weights[3], 0.01, datas, 0, 2, 100, 0.2)
-lms_classifier.learn()
+#lms_classifier.learn()
+
+plot_datas(datas, ['ro', 'bo', 'ko'])
+plt.legend(loc="upper right")
+plt.show()
